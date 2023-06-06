1e49c4d40febd585adf9d2e25da28edbd0c9519f
Samuel Woszak <135650426+SamuelWoszak@users.noreply.github.com>
2023-06-05 21:40:04 -0300

    Update Second Degree Equation.py
    an error in logic when tried to see multiply of nominator and denominator

--- Math/Second Degree Equation.py
+++ Math/Second Degree Equation.py
@@ -1,4 +1,4 @@
-#This code read complex, real, frac, not exact roots and exact equations
+#this code shows when isnt a exact root, complex or real roots, frac result also int results
 import cmath, math
 
 class EquationSecondDegree:
@@ -8,7 +8,7 @@ class EquationSecondDegree:
     if delta > 0:
       if math.sqrt(delta) == int(math.sqrt(delta)):
         if (-b + math.sqrt(delta)) % 2 == 0: #simplify by 2
-          if ((-b + math.sqrt(delta))%2) % a == 0 and (-b - math.sqrt(delta)) % a == 0: #simplify by a because they r multiply 
+          if ((-b + math.sqrt(delta))//2) % a == 0 and ((-b - math.sqrt(delta))// 2) % a == 0: #simplify by a because they r multiply 
               x1 = (-b + math.sqrt(delta))/(2 * a)
               x2 = (-b - math.sqrt(delta))/(2 * a) 
           else:
@@ -27,7 +27,7 @@ class EquationSecondDegree:
       print(f'As raízes são {x1} e {x2}!')
     elif delta == 0: #if there's a discriminant = 0 so x1 = x2
       if b % 2 == 0: #if b is pair simplify the frac
-        if (b % 2) % a == 0: #if b is multiply of a so simplify
+        if (b // 2) % a == 0: #if b is multiply of a so simplify
           x1 = -b/(2 * a)
         else: #if its not multiply of a so stay in frac mode
           x1 = f'{-b/2}/{a}'
@@ -41,33 +41,40 @@ class EquationSecondDegree:
     else:
       z = complex(b, delta)
       if z.real % 2 == 0 and z.imag % 4 == 0: #check if both are pair to simplify the frac
-        if (z.real%2) % a == 0 and (z.imag %4) % (a ** 2) == 0: #check if both are multiply of "a" to simplify the frac (it was simplfied early)
+        if ((z.real // 2) % a) == 0 and ((z.imag // 4) % (a ** 2)) == 0: #check if both are multiply of "a" to simplify the frac (it was simplfied early)
           if math.sqrt(abs(delta)) == int(math.sqrt(abs(delta))): #see if the root is exactly to simplify
             x1 = f'{-z.real/(2 * a)} + i{math.sqrt(abs(z.imag))/(4 * (a ** 2))}'#complex notation
             x2 = f'{-z.real/(2 * a)} - i{math.sqrt(abs(z.imag))/(4 * (a ** 2))}'#complex notation
+            print(f'1')
           else:
             x1 = f'{-z.real/(2 * a)} + i√{(abs(z.imag))/(4 * (a ** 2))}' #complex notation with sqrt() symbol bc discriminant isnt exact
             x2 = f'{-z.real/(2 * a)} - i√{(abs(z.imag))/(4 * (a ** 2))}' #complex notation with sqrt() symbol bc discriminant isnt exact   
+            print(f'2 {math.sqrt(abs(delta))}, {int(math.sqrt(abs(delta)))}, {(z.imag)}')
         else:
           if math.sqrt(abs(delta)) == int(math.sqrt(abs(delta))): #see if the root is exactly to simplify
-            x1 = f'{-z.real/(2 * a)} + i{math.sqrt(abs(z.imag))/(4 * (a ** 2))}'#complex notation
-            x2 = f'{-z.real/(2 * a)} - i{math.sqrt(abs(z.imag))/(4 * (a ** 2))}'#complex notation
+            x1 = f'({-z.real/(2)} + i{math.sqrt(abs(z.imag))/(4)})/{a}'#complex notation
+            x2 = f'({-z.real/(2)} - i{math.sqrt(abs(z.imag))/(4)})/{a}'#complex notation
+            print('3')
           else:
-            x1 = f'{-z.real/(2 * a)} + i√{(abs(z.imag))/(4 * (a ** 2))}'#complex notation
-            x2 = f'{-z.real/(2 * a)} - i√{(abs(z.imag))/(4 * (a ** 2))}'#complex notation
+            x1 = f'({-z.real/(2)} + i√{(abs(z.imag))/(4)})/{a}'#complex notation
+            x2 = f'({-z.real/(2)} - i√{(abs(z.imag))/(4)})/{a}'#complex notation
+            print('4')
           
       if z.real % 2 != 0 or z.imag % 4 != 0: #odd a and b (saying a + bi form)
         if z.real % a == 0 and z.imag % (a ** 2) == 0: #if they r multiply of a
           if math.sqrt(abs(delta)) == int(math.sqrt(abs(delta))):  #if exact discriminant
             x1 = f'(-{z.real/a} + i{math.sqrt(abs(z.imag))/(a ** 2)})/2' #print root simplified by a
             x2 = f'(-{z.real/a} - i{math.sqrt(abs(z.imag))/(a ** 2)})/2' #print root simplified by a
+            print('5')
           else:
             x1 = f'(-{z.real/a} + i√{(abs(z.imag))/(a ** 2)})/2' #print root simplified by a but discriminant isnt exact
-            x2 = f'(-{z.real/a} - i√{(abs(z.imag))/(a ** 2)})/2' #print root simplified by a but discriminant isnt exact            
+            x2 = f'(-{z.real/a} - i√{(abs(z.imag))/(a ** 2)})/2' #print root simplified by a but discriminant isnt exact     
+            print('6')       
         else:
           if math.sqrt(abs(delta)) == int(math.sqrt(abs(delta))):
             x1 = f'(-{z.real} + i{math.sqrt(abs(z.imag))})/{2 * a}' #they are not  divisible by a and 2 and exact root
             x2 = f'(-{z.real} - i{math.sqrt(abs(z.imag))})/{2 * a}' #they are not  divisible by a and 2 and exact root
+            print('7')
       print(f'As suas raízes são {x1} e {x2}!')
 equationseconddegree = EquationSecondDegree()
-equationseconddegree.SolveEquation()
+equationseconddegree.SolveEquation(3, 4, 2)
