1534ad61959fb09efcfe46f17a2e1cc896fc5ba8
Samuel Woszak <135650426+SamuelWoszak@users.noreply.github.com>
2023-06-07 22:26:56 -0300

    add some stuff

--- App Ideas/Begginer/Bin2Dec.py
+++ App Ideas/Begginer/Bin2Dec.py
@@ -6,6 +6,7 @@ for c in number:
   if c == '1':
     total += 2**counter
   counter += 1
-  
+  if c != '1' or c != '0':
+    print('You put sometihng different than 0 or 1, please insert only 1 or 0!')
+    quit()
 print(total)
-  
